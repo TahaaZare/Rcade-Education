261f582f3258d0e5dff6a9f349ec1ca2480e119b
TahaaZare <tahaazre@gmail.com>
2024-05-04 17:00:00 +0330

    database #02

---
+++ database/migrations/2024_05_04_132452_create_courses_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('master_id')->constrained('users');
+
+            $table->string('name');
+            $table->string('summary',120);
+            $table->longText('description');
+            $table->text('image');
+            $table->decimal('price', 20, 3);
+            $table->string('slug')->unique();
+            $table->string('short_link')->unique()->nullable();
+            $table->tinyInteger('course_level')->default(0)->comment('0 : easy, 1 : normal, 2 : hard, 3 : pro');
+            $table->tinyInteger('course_status')->default(0)->comment('0 : comming soon, 1 : recording, 2 : finished');
+            $table->tinyInteger('status')->default(0);
+            $table->tinyInteger('comment_able')->default(0);
+            $table->text('tags');
+
+            $table->text('meta_keywords');
+            $table->text('meta_description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
